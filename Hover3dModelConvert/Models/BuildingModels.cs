d01ec6404deff9fa4872f4367a416e52c7399592
artyom <kapec.art@yandex.ru>
2023-07-31 16:39:37 +0600

    Добавьте файлы проекта.

---
+++ Hover3dModelConvert/Models/BuildingModels.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hover3dModelConvert.Models
+{
+    // Примечание. Для запуска созданного кода может потребоваться NET Framework версии 4.5 или более поздней версии и .NET Core или Standard версии 2.0 или более поздней.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class DATA_EXPORT
+    {
+
+        private DATA_EXPORTLOCATION lOCATIONField;
+
+        private DATA_EXPORTSTRUCTURES sTRUCTURESField;
+
+        /// <remarks/>
+        public DATA_EXPORTLOCATION LOCATION
+        {
+            get
+            {
+                return this.lOCATIONField;
+            }
+            set
+            {
+                this.lOCATIONField = value;
+            }
+        }
+
+        /// <remarks/>
+        public DATA_EXPORTSTRUCTURES STRUCTURES
+        {
+            get
+            {
+                return this.sTRUCTURESField;
+            }
+            set
+            {
+                this.sTRUCTURESField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class DATA_EXPORTLOCATION
+    {
+
+        private string addressField;
+
+        private decimal latField;
+
+        private decimal longField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string address
+        {
+            get
+            {
+                return this.addressField;
+            }
+            set
+            {
+                this.addressField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal lat
+        {
+            get
+            {
+                return this.latField;
+            }
+            set
+            {
+                this.latField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal @long
+        {
+            get
+            {
+                return this.longField;
+            }
+            set
+            {
+                this.longField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class DATA_EXPORTSTRUCTURES
+    {
+
+        private DATA_EXPORTSTRUCTURESROOF rOOFField;
+
+        /// <remarks/>
+        public DATA_EXPORTSTRUCTURESROOF ROOF
+        {
+            get
+            {
+                return this.rOOFField;
+            }
+            set
+            {
+                this.rOOFField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class DATA_EXPORTSTRUCTURESROOF
+    {
+
+        private DATA_EXPORTSTRUCTURESROOFFACE[] fACESField;
+
+        private DATA_EXPORTSTRUCTURESROOFLINE[] lINESField;
+
+        private DATA_EXPORTSTRUCTURESROOFPOINT[] pOINTSField;
+
+        private string idField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("FACE", IsNullable = false)]
+        public DATA_EXPORTSTRUCTURESROOFFACE[] FACES
+        {
+            get
+            {
+                return this.fACESField;
+            }
+            set
+            {
+                this.fACESField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("LINE", IsNullable = false)]
+        public DATA_EXPORTSTRUCTURESROOFLINE[] LINES
+        {
+            get
+            {
+                return this.lINESField;
+            }
+            set
+            {
+                this.lINESField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("POINT", IsNullable = false)]
+        public DATA_EXPORTSTRUCTURESROOFPOINT[] POINTS
+        {
+            get
+            {
+                return this.pOINTSField;
+            }
+            set
+            {
+                this.pOINTSField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class DATA_EXPORTSTRUCTURESROOFFACE
+    {
+
+        private DATA_EXPORTSTRUCTURESROOFFACEPOLYGON pOLYGONField;
+
+        private string idField;
+
+        private string typeField;
+
+        private string nameField;
+
+        private string childrenField;
+
+        /// <remarks/>
+        public DATA_EXPORTSTRUCTURESROOFFACEPOLYGON POLYGON
+        {
+            get
+            {
+                return this.pOLYGONField;
+            }
+            set
+            {
+                this.pOLYGONField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string type
+        {
+            get
+            {
+                return this.typeField;
+            }
+            set
+            {
+                this.typeField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string children
+        {
+            get
+            {
+                return this.childrenField;
+            }
+            set
+            {
+                this.childrenField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class DATA_EXPORTSTRUCTURESROOFFACEPOLYGON
+    {
+
+        private string idField;
+
+        private string pathField;
+
+        private decimal sizeField;
+
+        private bool sizeFieldSpecified;
+
+        private string pitchField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string path
+        {
+            get
+            {
+                return this.pathField;
+            }
+            set
+            {
+                this.pathField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal size
+        {
+            get
+            {
+                return this.sizeField;
+            }
+            set
+            {
+                this.sizeField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public bool sizeSpecified
+        {
+            get
+            {
+                return this.sizeFieldSpecified;
+            }
+            set
+            {
+                this.sizeFieldSpecified = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string pitch
+        {
+            get
+            {
+                return this.pitchField;
+            }
+            set
+            {
+                this.pitchField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class DATA_EXPORTSTRUCTURESROOFLINE
+    {
+
+        private string idField;
+
+        private string typeField;
+
+        private string pathField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string type
+        {
+            get
+            {
+                return this.typeField;
+            }
+            set
+            {
+                this.typeField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string path
+        {
+            get
+            {
+                return this.pathField;
+            }
+            set
+            {
+                this.pathField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class DATA_EXPORTSTRUCTURESROOFPOINT
+    {
+
+        private string idField;
+
+        private string dataField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string data
+        {
+            get
+            {
+                return this.dataField;
+            }
+            set
+            {
+                this.dataField = value;
+            }
+        }
+    }
+
+
+}
---
+++ Hover3dModelConvert/Program.cs
@@ -0,0 +1,199 @@
+﻿using Hover3dModelConvert.Models;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text.Json.Nodes;
+using System.Xml.Serialization;
+
+namespace Hover3dModelConvert // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static Dictionary<int, int> pointIndexes = new Dictionary<int, int>();
+
+        static Dictionary<string, int> pointStringIndexes = new Dictionary<string, int>();
+
+
+        static void Main(string[] args)
+        {
+
+            // BuildFromJson();
+            BuildFromXML();
+        }
+
+        private static void BuildFromXML()
+        {
+            FileStream fileStream = File.OpenWrite("hoverModelFromXml.obj");
+            StreamWriter streamWriter = streamWriter = new StreamWriter(
+                                   fileStream,
+                                   encoding: null,
+                                   bufferSize: -1,
+                                   leaveOpen: true);
+
+
+
+            var hoverJson = File.ReadAllText("3dmodel2.xml");
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(DATA_EXPORT));
+
+            DATA_EXPORT? data;
+            // десериализуем объект
+            using (FileStream fs = new FileStream("3dmodel2.xml", FileMode.OpenOrCreate))
+            {
+                data = xmlSerializer.Deserialize(fs) as DATA_EXPORT;
+            }
+            int i = 1;
+            foreach (var point in data.STRUCTURES.ROOF.POINTS)
+            {
+                pointStringIndexes.Add(point.id, i);
+
+                streamWriter.WriteLine($"v {point.data.Replace(",", " ").Replace(".", ",")}");
+                i++;
+            }
+
+            foreach (var face in data.STRUCTURES.ROOF.FACES)
+            {
+                streamWriter.WriteLine($"g {face.type}");
+
+                var lines = face.POLYGON.path.Split(',');
+
+                List<int> facePoints = new List<int>();
+                foreach (var line in lines)
+                {
+                    var points = data.STRUCTURES.ROOF.LINES.FirstOrDefault(x => x.id == line).path.Split(',');
+
+                    facePoints.AddRange(points.Select(x => pointStringIndexes[x]));
+                }
+
+                streamWriter.WriteLine($"f {string.Join(' ', facePoints)}");
+
+            }
+        }
+
+
+
+        private static void BuildFromJson()
+        {
+
+
+            var hoverJson = File.ReadAllText("3dmodel1.json");
+            var hoverRoot = JsonNode.Parse(hoverJson).AsObject();
+            FileStream fileStream = File.OpenWrite("hoverModel.obj");
+            StreamWriter streamWriter = new StreamWriter(
+                                   fileStream,
+                                   encoding: null,
+                                   bufferSize: -1,
+                                   leaveOpen: true);
+            var points = hoverRoot["points"];
+            var facades = hoverRoot["facades"];
+            var edges = hoverRoot["edges"];
+
+
+            WriteVerticesListAsync(streamWriter, points.AsObject());
+            streamWriter.WriteLine();
+
+            var groups = hoverRoot["groups"].AsObject();
+
+            var maxPointIndex = 0;
+
+            var groupFacades = new Dictionary<string, IEnumerable<int>>();
+
+            //var opening = new[] { "opening_tops", "opening_sides", "opening_bottoms" };
+            var opening = new[] { "outside_corners", "level_bases", "perimeter" };
+
+
+            // streamWriter.WriteLine($"g opening");
+            List<int> GroupFaces = new List<int>();
+
+
+
+            foreach (var facade in facades.AsObject())
+            {
+                var edgesTypes = facade.Value["edges"].AsArray().Select(x => x.AsObject()["type"].ToString()).Distinct();
+
+
+                foreach (var type in edgesTypes)
+                {
+                    streamWriter.WriteLine($"g {type}");
+
+                    var edgesIds = facade.Value["edges"].AsArray()
+                    .Where(x => x.AsObject()["type"].ToString() == type)
+                    .Select(x => x.AsObject()["id"].ToString());
+
+                    if (!edgesIds.Any())
+                        continue;
+
+                    var faces = edges.AsObject().Where(x => edgesIds.Contains(x.Key))
+
+                          .Select(x => x.Value["points"]).SelectMany(x => x.AsArray())
+                          .Select(x => pointIndexes[Convert.ToInt32(x.ToString())]);
+
+                    streamWriter.WriteLine($"f {string.Join(' ', faces)}");
+
+                    streamWriter.WriteLine();
+                }
+
+            }
+
+            //  streamWriter.WriteLine($"f {string.Join(' ', faces)}");
+
+            // BuildByGroups(streamWriter, facades, edges, groups, maxPointIndex);
+
+            Console.WriteLine("Max point index " + maxPointIndex);
+        }
+
+        private static int BuildByGroups(StreamWriter streamWriter, JsonNode? facades, JsonNode? edges, JsonObject groups, int maxPointIndex)
+        {
+            foreach (var group in groups)
+            {
+                streamWriter.WriteLine($"g {group.Key}");
+
+                var groupFacadeIds = group.Value["facade_ids"].AsArray();
+
+                foreach (var groupFacadeId in groupFacadeIds)
+                {
+                    var groupFacade = facades.AsObject()[groupFacadeId.ToString()].AsObject();
+
+                    var edgesIds = groupFacade["edges"].AsArray()
+                        .Where(x => x.AsObject()["type"].ToString() != "opening_tops")
+                        .Where(x => x.AsObject()["type"].ToString() != "opening_sides")
+                        .Where(x => x.AsObject()["type"].ToString() != "opening_bottoms")
+
+                        .Select(x => x.AsObject()["id"].ToString());
+
+                    var faces = edges.AsObject().Where(x => edgesIds.Contains(x.Key))
+                        .Select(x => x.Value["points"]).SelectMany(x => x.AsArray())
+                        .Select(x => pointIndexes[Convert.ToInt32(x.ToString())]);
+
+                    if (!faces.Any())
+                        continue;
+
+                    maxPointIndex = faces.Max();
+
+                    streamWriter.WriteLine($"f {string.Join(' ', faces)}");
+                }
+
+                streamWriter.WriteLine();
+            }
+
+            return maxPointIndex;
+        }
+
+        private static void WriteVerticesListAsync(StreamWriter output, JsonObject vertices)
+        {
+            int i = 1;
+            foreach (var vertex in vertices)
+            {
+                var position = vertex.Value["position"];
+
+                output.WriteLine($"v {position[0].ToString().Replace('.', ',')} {position[1].ToString().Replace('.', ',')} {position[2].ToString().Replace('.', ',')}");
+
+                pointIndexes.Add(Convert.ToInt32(vertex.Key), i);
+
+                i++;
+            }
+
+            Console.WriteLine("Count of Vertix:" + vertices.Count());
+        }
+    }
+}
